e8a1b30586c3d02f0c8380b99c89d899e1ca733a
Giovanni Marino <GiovaMarino1@gmail.com>
2023-02-18 13:30:58 -0500

    made initialDraw generic to the player using it

--- src/App.js
+++ src/App.js
@@ -3,31 +3,44 @@ import cards from "./data/cards.json";
 // import users from "./data/users.json";
 
 let n = Math.floor(Math.random() * cards.length);
-console.log(n);
+// console.log(n);
 let usersCards = [];
+let botsCards = [];
+let cardsInPlay = [];
 
-let initialDraw = () => {
+let initialDraw = (player) => {
   for (let i = 0; i < 7; i++) {
     let cardSelection = Math.floor(Math.random() * cards.length);
     // if card is already in hand:
-    if (usersCards.includes(cardSelection)) {
+    if (cardsInPlay.includes(cardSelection)) {
       // get new card. check if already in hand
-      while (usersCards.includes(cardSelection)) {
+      while (cardsInPlay.includes(cardSelection)) {
         cardSelection = Math.floor(Math.random() * cards.length);
       }
     }
-    usersCards.push(cardSelection);
+    player.push(cardSelection);
+    cardsInPlay.push(cardSelection);
   }
-  console.log(usersCards);
+  console.log(`cards in play: ${cardsInPlay}`);
 };
-initialDraw();
+initialDraw(usersCards);
+initialDraw(botsCards);
+console.log(usersCards);
+console.log(botsCards);
 
 function App() {
   return (
     <div className="App">
-      {usersCards.map((card) => {
-        return <img src={cards[card].img} alt="" />;
-      })}
+      <div>
+        {usersCards.map((card) => {
+          return <img src={cards[card].img} alt="" />;
+        })}
+      </div>
+      <div>
+        {botsCards.map((card) => {
+          return <img src={cards[card].img} alt="" />;
+        })}
+      </div>
     </div>
   );
 }
