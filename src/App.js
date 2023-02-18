fac6e4d6be6e49a91d7298296949b77bc0507a47
Giovanni Marino <GiovaMarino1@gmail.com>
2023-02-17 23:31:06 -0500

    worked on initialDraw function

--- src/App.js
+++ src/App.js
@@ -2,26 +2,25 @@ import "./App.css";
 import cards from "./data/cards.json";
 // import users from "./data/users.json";
 
-// console.log(cards[1].test[Math.floor(Math.random() * cards[1].test.length)]);
-// console.log(cards[1].test[Math.floor(Math.random() * cards[1].test.length)]);
-
 let n = Math.floor(Math.random() * cards.length);
 console.log(n);
 
-let startDraw = () => {
+let initialDraw = () => {
   let usersCards = [];
   for (let i = 0; i < 7; i++) {
     let cardSelection = Math.floor(Math.random() * cards.length);
-
-    // push into userCards if no duplicates
-    if (!usersCards.includes(cardSelection)) {
-      usersCards.push(cardSelection);
+    // if card is already in hand:
+    if (usersCards.includes(cardSelection)) {
+      // get new card. check if already in hand
+      while (usersCards.includes(cardSelection)) {
+        cardSelection = Math.floor(Math.random() * cards.length);
+      }
     }
+    usersCards.push(cardSelection);
   }
   console.log(usersCards);
-  // users[0].hand.push(usersCards);
 };
-startDraw();
+initialDraw();
 
 function App() {
   return (
